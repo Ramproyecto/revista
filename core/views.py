25ff707ae5b5136bbbf091f6fa7936a7ffcce18f
danielvvera <dvtareas@gmail.com>
2023-11-21 17:34:40 -0300

    backgrounds

--- core/views.py
+++ core/views.py
@@ -3,13 +3,25 @@ from django.shortcuts import render
 # Create your views here.
 
 def Home(request):
-    return render(request, "core/home.html")
+    context = {
+        'ruta_actual': request.path,
+    }
+    return render(request, "core/home.html", context)
 
 def AboutUs(request):
-    return render(request, "core/about.html")
+    context = {
+        'ruta_actual': request.path,
+    }
+    return render(request, "core/about.html", context)
 
 def Frequent(request):
-    return render(request, "core/FAQ.html")
+    context = {
+        'ruta_actual': request.path,
+    }
+    return render(request, "core/FAQ.html", context)
 
 def Magazine(request):
-    return render(request, "core/revista.html")
+    context = {
+        'ruta_actual': request.path,
+    }
+    return render(request, "core/revista.html", context)
